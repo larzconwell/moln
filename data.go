d6595caed7a9dd0641d8e83560d1c737a2e42d9f
Larz Conwell <larzconwell@gmail.com>
2013-06-24 22:24:02 -0400

    Refactor DB management, not complete yet. Token and Device data sets are complete.
    Refactor API layout, changes were mostly infomational about responses
    and authorization.
    
    Refactor backend data layout, changes were required to make it easier to
    access devices, and ensure conflicts for device names don't occur across
    users
    
    Remove code for user create, until the data modeling is complete.

--- data.go
+++ data.go
@@ -1,187 +1,410 @@
 package main
 
 import (
-	"code.google.com/p/go.crypto/bcrypt"
 	"errors"
 	"github.com/garyburd/redigo/redis"
 	"github.com/nu7hatch/gouuid"
 )
 
 // Create the error slice if nil and append the error
-func appendOrCreateErrors(errs []error, err error) []error {
+func createAndAppendErrors(errs []error, err ...error) []error {
 	if errs == nil {
 		errs = make([]error, 0)
 	}
-	errs = append(errs, err)
-	return errs
+	return append(errs, err...)
 }
 
-// User is a single user with an encrypted password
+// Data implements access to update specific sections of the database and provide validation
+// for the data being set
+type Data interface {
+	// Validate should validate the data to be set, a nil error slice means
+	// no failed validations
+	Validate() ([]error, error)
+	// Save should save the data set to the database
+	Save() error
+	ValidateAndSave() ([]error, error)
+	// Delete should remove the data from the database
+	Delete() error
+}
+
+// User implements Data allowing updates to a user object
 type User struct {
-	Username string `redis:"username"`
-	Password []byte `redis:"password"`
+	Name     string   `redis:"name" json:"name"`
+	Password string   `redis:"password" json:"-"`
+	Devices  []string `redis:"-" json:"devices"`
 }
 
-// Create a new user and hashes the given password
-func NewUser(username string, password []byte) (*User, error) {
-	user := &User{Username: username}
+func FindUser(name string) (*User, error) { return nil, nil }
 
-	if username == "" || len(password) <= 0 {
-		return nil, errors.New("Username or password was not given")
-	}
+/*
+ * Device
+ */
 
-	passHash, err := bcrypt.GenerateFromPassword(password, -1)
-	if err != nil {
-		return nil, err
-	}
-	user.Password = passHash
-
-	return user, nil
-}
-
-// Validate the user data, the slice of errors are the validation errors. Nil if valid
-func (user *User) Validate() ([]error, error) {
-	var errs []error
-
-	if user.Username == "" {
-		errs = appendOrCreateErrors(errs, errors.New("User: Username is empty"))
-	}
-
-	if len(user.Password) <= 0 {
-		errs = appendOrCreateErrors(errs, errors.New("User: Password is empty"))
-	}
-
-	// Check if the user exists
-	exists, err := redis.Bool(DB.Do("exists", "user:"+user.Username))
-	if err != nil {
-		return nil, err
-	}
-
-	if exists {
-		errs = appendOrCreateErrors(errs, errors.New("User: Username "+user.Username+" exists"))
-	}
-
-	return errs, nil
-}
-
-// Save the users data
-func (user *User) Save() error {
-	// Update user fields
-	_, err := DB.Do("hmset", redis.Args{}.Add("user:"+user.Username).AddFlat(user)...)
-	if err != nil {
-		return err
-	}
-
-	// Get the users device names
-	deviceNames, err := DB.Do("smembers", "user:"+user.Username+":devices")
-	if err != nil {
-		return err
-	}
-
-	for _, n := range deviceNames.([]interface{}) {
-		name := string(n.([]byte))
-
-		// Set the devices user
-		_, err := DB.Do("hset", "device:"+name, "user", user.Username)
-		if err != nil {
-			return err
-		}
-
-		// Get the devices token
-		t, err := DB.Do("hget", "device:"+name, "token")
-		if err != nil {
-			return err
-		}
-		token := string(t.([]byte))
-
-		// Set the tokens user
-		_, err = DB.Do("hset", "token:"+token, "user", user.Username)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// Add a new device to the user
-func (user *User) AddDevice(name string) (*Device, error) {
-	return NewDevice(name, user.Username)
-}
-
-// Device is single device with a unique token and a username to link to an owner
+// Device implements Data allowing updates to a users device
 type Device struct {
-	Name     string `redis:"name"`
-	Token    string `redis:"token"`
-	Username string `redis:"user"`
+	Name  string `redis:"name" json:"name"`
+	Token string `redis:"token" json:"token"`
+	User  string `redis:"user" json:"-"`
+	new   bool
+	token *Token
+	user  *User
 }
 
-// Create a new device with a unique token
-func NewDevice(name, username string) (*Device, error) {
-	device := &Device{Name: name, Username: username}
-
-	if name == "" || username == "" {
-		return nil, errors.New("Device name or username was not given")
-	}
-
-	id, err := uuid.NewV5(uuid.NamespaceURL, []byte(name+username))
+// NewDevice creates a new device with a unique token
+func NewDevice(name, user string) (*Device, error) {
+	device := &Device{Name: name, User: user, new: true}
+	token, err := NewToken(name, user)
 	if err != nil {
 		return nil, err
 	}
-	device.Token = id.String()
 
-	return device, nil
+	device.SetToken(token)
+	return device, err
 }
 
-// Validate the device data, the slice of errors are the validation errors. Nil if valid
+// FindDevice finds the device in the DB
+func FindDevice(name, user string) (*Device, error) {
+	reply, err := DB.Do("hgetall", "user:"+user+":device:"+name)
+	if err != nil {
+		return nil, err
+	}
+
+	device := &Device{}
+	err = redis.ScanStruct(reply.([]interface{}), device)
+	return device, err
+}
+
+// DeleteDevice deletes the device in the DB
+func DeleteDevice(name, user string) (*Device, error) {
+	device, err := FindDevice(name, user)
+	if err != nil {
+		return nil, err
+	}
+
+	return device, device.Delete()
+}
+
+// DeviceExists checks if the device exsts in the DB
+func DeviceExists(name, user string) (bool, error) {
+	return redis.Bool(DB.Do("exists", "user:"+user+":device:"+name))
+}
+
+func (device *Device) String() string {
+	return device.Name
+}
+
+// SetToken sets the underlying token
+func (device *Device) SetToken(token *Token) {
+	device.Token = token.Value
+	device.token = token
+}
+
+// SetUser sets the underlying user
+func (device *Device) SetUser(user *User) {
+	device.User = user.Name
+	device.user = user
+
+	if device.token != nil {
+		device.token.SetUser(user)
+	}
+}
+
+// FindToken finds the devices token, using the set token if exists
+func (device *Device) FindToken() (*Token, error) {
+	var err error
+	token := device.token
+
+	if token == nil {
+		token, err = FindToken(device.Token)
+	}
+
+	if token != nil {
+		device.SetToken(token)
+		token.SetDevice(device)
+
+		if device.user != nil {
+			token.SetUser(device.user)
+		}
+	}
+
+	return token, err
+}
+
+// FindUser finds the devices user, using the set user if exists
+func (device *Device) FindUser() (*User, error) {
+	var err error
+	user := device.user
+
+	if user == nil {
+		user, err = FindUser(device.User)
+	}
+
+	if user != nil {
+		device.SetUser(user)
+		// TODO: How to add a users device?
+	}
+
+	return user, err
+}
+
+// Validate ensures the device name is not taken for the user and validates the devices token
 func (device *Device) Validate() ([]error, error) {
+	var (
+		errs []error
+		err  error
+	)
+
+	// Only check the devices name uniqueness if it's a new item
+	if device.new {
+		exists, err := redis.Bool(DB.Do("exists", "user:"+device.User+":device:"+device.Name))
+		if err != nil {
+			return errs, err
+		}
+		if exists {
+			errs = createAndAppendErrors(errs, errors.New("Device: name already exists"))
+		}
+	}
+
+	token := device.token
+	if token == nil {
+		token, err = device.FindToken()
+		if err != nil {
+			return errs, err
+		}
+	}
+
+	// Validate token appending errors
+	es, err := token.Validate()
+	if err != nil {
+		return errs, err
+	}
+	if es != nil {
+		errs = createAndAppendErrors(errs, es...)
+	}
+
+	return errs, err
+}
+
+// Save writes the device and it's token to the DB
+func (device *Device) Save() error {
+	var err error
+
+	token := device.token
+	if token == nil {
+		token, err = device.FindToken()
+		if err != nil {
+			return err
+		}
+	}
+
+	if token != nil {
+		err = token.Save()
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = DB.Do("hmset", redis.Args{}.Add("user:"+device.User+":device:"+device.Name).AddFlat(device)...)
+	if err == nil {
+		// Unset new since saving causes the item to not be new anymore
+		device.new = false
+	}
+
+	return err
+}
+
+// ValidateAndSave validates and saves the data if valid
+func (device *Device) ValidateAndSave() ([]error, error) {
+	errs, err := device.Validate()
+	if errs == nil && err == nil {
+		err = device.Save()
+	}
+
+	return errs, err
+}
+
+// Delete the device and it's token from the DB
+func (device *Device) Delete() error {
+	var err error
+
+	token := device.token
+	if token == nil {
+		token, err = device.FindToken()
+		if err != nil {
+			return err
+		}
+	}
+
+	if token != nil {
+		err = token.Delete()
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = DB.Do("del", "user:"+device.User+":device:"+device.Name)
+	if err == nil {
+		// Set new to true, so validation will check uniqueness
+		device.new = true
+	}
+
+	return err
+}
+
+/*
+ * Token
+ */
+
+// Token implements Data allowing manipulation of the token linking to users and devices
+type Token struct {
+	Value  string `redis:"-"`
+	Device string `redis:"device"`
+	User   string `redis:"user"`
+	new    bool
+	device *Device
+	user   *User
+}
+
+// NewToken creates a new unique token
+func NewToken(device, user string) (*Token, error) {
+	token := &Token{Device: device, User: user, new: true}
+	err := token.UpdateValue()
+
+	return token, err
+}
+
+// FindToken finds the token in the DB
+func FindToken(t string) (*Token, error) {
+	reply, err := DB.Do("hgetall", "token:"+t)
+	if err != nil {
+		return nil, err
+	}
+
+	token := &Token{Value: t}
+	err = redis.ScanStruct(reply.([]interface{}), token)
+	return token, err
+}
+
+// DeleteToken deletes the token in the DB
+func DeleteToken(t string) (*Token, error) {
+	token, err := FindToken(t)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, token.Delete()
+}
+
+// TokenExists checks if the token exists in the DB
+func TokenExists(t string) (bool, error) {
+	return redis.Bool(DB.Do("exists", "token:"+t))
+}
+
+func (token *Token) String() string {
+	return token.Value
+}
+
+// UpdateToken creates a uuid for the tokens device and user
+func (token *Token) UpdateValue() error {
+	id, err := uuid.NewV5(uuid.NamespaceURL, []byte(token.Device+token.User))
+	if id != nil {
+		token.Value = id.String()
+	}
+
+	return err
+}
+
+// SetDevice sets the underlying device
+func (token *Token) SetDevice(device *Device) {
+	token.Device = device.Name
+	token.device = device
+}
+
+// SetUser sets the underlying user
+func (token *Token) SetUser(user *User) {
+	token.User = user.Name
+	token.user = user
+}
+
+// FindDevice finds the tokens device, returning the set device if exists
+func (token *Token) FindDevice() (*Device, error) {
+	var err error
+	device := token.device
+
+	if device == nil {
+		device, err = FindDevice(token.Device, token.User)
+	}
+
+	if device != nil {
+		token.SetDevice(device)
+		device.SetToken(token)
+	}
+
+	return device, err
+}
+
+// FindUser finds the tokens user, returning the set user if exists
+func (token *Token) FindUser() (*User, error) {
+	var err error
+	user := token.user
+
+	if user == nil {
+		user, err = FindUser(token.User)
+	}
+
+	if user != nil {
+		token.SetUser(user)
+	}
+
+	return user, err
+}
+
+// Validate ensures the tokens uniqueness
+func (token *Token) Validate() ([]error, error) {
 	var errs []error
 
-	if device.Name == "" {
-		errs = appendOrCreateErrors(errs, errors.New("Device: Name is empty"))
-	}
-
-	if device.Username == "" {
-		errs = appendOrCreateErrors(errs, errors.New("Device: Username is empty"))
-	}
-
-	// Check if the device name is a member of the users devices set
-	exists, err := redis.Bool(DB.Do("sismember", "user:"+device.Username+":devices", device.Name))
-	if err != nil {
-		return nil, err
-	}
-	if exists {
-		errs = appendOrCreateErrors(errs,
-			errors.New("Device: Device name "+device.Name+" already exists for "+device.Username))
-	}
-
-	// Check if the token exists
-	exists, err = redis.Bool(DB.Do("exists", "token:"+device.Token))
-	if err != nil {
-		return nil, err
-	}
-	if exists {
-		errs = appendOrCreateErrors(errs, errors.New("Device: Token "+device.Name+" exists"))
+	// Only check if the token exists if it's new
+	if token.new {
+		exists, err := redis.Bool(DB.Do("exists", "token:"+token.Value))
+		if err != nil {
+			return errs, err
+		}
+		if exists {
+			errs = createAndAppendErrors(errs, errors.New("Token: token already exists"))
+		}
 	}
 
 	return errs, nil
 }
 
-// Save the devices data
-func (device *Device) Save() error {
-	// Update device fields
-	_, err := DB.Do("hmset", redis.Args{}.Add("device:"+device.Name).AddFlat(device)...)
-	if err != nil {
-		return err
+// Save writes the token to the DB
+func (token *Token) Save() error {
+	_, err := DB.Do("hmset", redis.Args{}.Add("token:"+token.Value).AddFlat(token)...)
+	if err == nil {
+		// Unset new since saving causes the item to not be new anymore
+		token.new = false
 	}
 
-	// Set the token fields
-	_, err = DB.Do("hmset", "token:"+device.Token, "device", device.Name, "user", device.Username)
-	if err != nil {
-		return err
+	return err
+}
+
+// ValidateAndSave validates and saves if data is valid
+func (token *Token) ValidateAndSave() ([]error, error) {
+	errs, err := token.Validate()
+	if errs == nil && err == nil {
+		err = token.Save()
 	}
 
-	// Add the device to the users devices
-	_, err = DB.Do("sadd", "user:"+device.Username+":devices", device.Name)
+	return errs, err
+}
+
+// Delete removes the token from the database
+func (token *Token) Delete() error {
+	_, err := DB.Do("del", "token:"+token.Value)
+	if err == nil {
+		// Set new to true, so validation will check uniqueness
+		token.new = true
+	}
+
 	return err
 }
--- users.go
+++ users.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -15,57 +14,8 @@ func CreateUserHandler(rw http.ResponseWriter, req *http.Request) {
 		ErrResponse(rw, http.StatusInternalServerError, err)
 		return
 	}
-	params := req.PostForm
-	username := params.Get("username")
-	password := []byte(params.Get("password"))
-	deviceName := params.Get("devicename")
-
-	user, err := NewUser(username, password)
-	if err != nil {
-		ErrResponse(rw, http.StatusInternalServerError, err)
-		return
-	}
-
-	errs, err := user.Validate()
-	if err != nil {
-		ErrResponse(rw, http.StatusInternalServerError, err)
-		return
-	}
-	if errs != nil {
-		MultiErrResponse(rw, http.StatusBadRequest, errs)
-		return
-	}
-
-	err = user.Save()
-	if err != nil {
-		ErrResponse(rw, http.StatusInternalServerError, err)
-		return
-	}
-
-	device, err := user.AddDevice(deviceName)
-	if err != nil {
-		ErrResponse(rw, http.StatusInternalServerError, err)
-		return
-	}
-
-	errs, err = device.Validate()
-	if err != nil {
-		ErrResponse(rw, http.StatusInternalServerError, err)
-		return
-	}
-	if errs != nil {
-		MultiErrResponse(rw, http.StatusBadRequest, errs)
-		return
-	}
-
-	err = device.Save()
-	if err != nil {
-		ErrResponse(rw, http.StatusInternalServerError, err)
-		return
-	}
-
-	fmt.Fprintln(rw, user)
-	fmt.Fprintln(rw, device)
-
-	// Encode user and return it
+	//params := req.PostForm
+	//username := params.Get("username")
+	//password := []byte(params.Get("password"))
+	//deviceName := params.Get("devicename")
 }
