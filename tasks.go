8641c074651a987f2849c01ae3b46585fa1e9d00
Larz Conwell <larzconwell@gmail.com>
2013-10-09 14:56:56 -0400

    Complete GET /tasks and GET /tasks/{id}

---
+++ tasks.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/larzconwell/moln/httpextra"
+	"net/http"
+)
+
+func init() {
+	getTasks := &Route{"GetTasks", "/tasks", []string{"GET"}, GetTasksHandler}
+	getTask := &Route{"GetTask", "/tasks/{id}", []string{"GET"}, GetTaskHandler}
+
+	Routes = append(Routes, getTasks, getTask)
+}
+
+func GetTasksHandler(rw http.ResponseWriter, req *http.Request) {
+	user := Authenticate(rw, req)
+	if user == nil {
+		return
+	}
+	res := &httpextra.Response{rw, req}
+
+	tasks, err := DB.GetTasks(user.Name)
+	if err != nil {
+		res.Send(map[string]string{"error": err.Error()}, http.StatusInternalServerError)
+		return
+	}
+
+	res.Send(tasks, http.StatusOK)
+}
+
+func GetTaskHandler(rw http.ResponseWriter, req *http.Request) {
+	user := Authenticate(rw, req)
+	if user == nil {
+		return
+	}
+	id := mux.Vars(req)["id"]
+	res := &httpextra.Response{rw, req}
+
+	task, err := DB.GetTask(user.Name, id)
+	if err != nil {
+		res.Send(map[string]string{"error": err.Error()}, http.StatusInternalServerError)
+		return
+	}
+
+	if task == nil {
+		res.Send(map[string]string{"error": http.StatusText(http.StatusNotFound)}, http.StatusNotFound)
+		return
+	}
+
+	res.Send(task, http.StatusOK)
+}
